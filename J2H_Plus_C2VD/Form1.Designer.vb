0daf747d08e962a90d65f692213a54e9f950c026
Pablo Yeguech <pablo.ys@hotmail.com>
2016-12-06 17:48:27 -0600

    Interfaz sin Scintilla

--- J2H_Plus_C2VD/Form1.Designer.vb
+++ J2H_Plus_C2VD/Form1.Designer.vb
@@ -24,35 +24,234 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GenerarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ErroresToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NuevoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AbrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuardarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuardarComoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.txtRemplazar = New System.Windows.Forms.TextBox()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnRemplazar = New System.Windows.Forms.Button()
+        Me.btnRemplazarT = New System.Windows.Forms.Button()
+        Me.lblCoincidencia = New System.Windows.Forms.Label()
+        Me.lblLinCol = New System.Windows.Forms.Label()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.MenuStrip1.SuspendLayout()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.GenerarToolStripMenuItem, Me.ErroresToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(871, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(893, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'ArchivoToolStripMenuItem
         '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoToolStripMenuItem, Me.AbrirToolStripMenuItem, Me.GuardarToolStripMenuItem, Me.GuardarComoToolStripMenuItem, Me.SalirToolStripMenuItem})
         Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
         Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
         Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'GenerarToolStripMenuItem
+        '
+        Me.GenerarToolStripMenuItem.Name = "GenerarToolStripMenuItem"
+        Me.GenerarToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.GenerarToolStripMenuItem.Text = "Generar"
+        '
+        'ErroresToolStripMenuItem
+        '
+        Me.ErroresToolStripMenuItem.Name = "ErroresToolStripMenuItem"
+        Me.ErroresToolStripMenuItem.Size = New System.Drawing.Size(55, 20)
+        Me.ErroresToolStripMenuItem.Text = "Errores"
+        '
+        'NuevoToolStripMenuItem
+        '
+        Me.NuevoToolStripMenuItem.Name = "NuevoToolStripMenuItem"
+        Me.NuevoToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.NuevoToolStripMenuItem.Text = "Nuevo"
+        '
+        'AbrirToolStripMenuItem
+        '
+        Me.AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        Me.AbrirToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.AbrirToolStripMenuItem.Text = "Abrir"
+        '
+        'GuardarToolStripMenuItem
+        '
+        Me.GuardarToolStripMenuItem.Name = "GuardarToolStripMenuItem"
+        Me.GuardarToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.GuardarToolStripMenuItem.Text = "Guardar"
+        '
+        'GuardarComoToolStripMenuItem
+        '
+        Me.GuardarComoToolStripMenuItem.Name = "GuardarComoToolStripMenuItem"
+        Me.GuardarComoToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.GuardarComoToolStripMenuItem.Text = "Guardar como..."
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.SplitContainer1.Location = New System.Drawing.Point(12, 27)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.SplitContainer2)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.lblLinCol)
+        Me.SplitContainer1.Size = New System.Drawing.Size(869, 412)
+        Me.SplitContainer1.SplitterDistance = 366
+        Me.SplitContainer1.TabIndex = 1
+        '
+        'SplitContainer2
+        '
+        Me.SplitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        '
+        'SplitContainer2.Panel1
+        '
+        Me.SplitContainer2.Panel1.Controls.Add(Me.TabControl1)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.Controls.Add(Me.lblCoincidencia)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.btnRemplazarT)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.btnRemplazar)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.btnBuscar)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.txtRemplazar)
+        Me.SplitContainer2.Panel2.Controls.Add(Me.txtBuscar)
+        Me.SplitContainer2.Size = New System.Drawing.Size(869, 366)
+        Me.SplitContainer2.SplitterDistance = 708
+        Me.SplitContainer2.TabIndex = 0
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Location = New System.Drawing.Point(22, 42)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(113, 20)
+        Me.txtBuscar.TabIndex = 0
+        '
+        'txtRemplazar
+        '
+        Me.txtRemplazar.Location = New System.Drawing.Point(22, 73)
+        Me.txtRemplazar.Name = "txtRemplazar"
+        Me.txtRemplazar.Size = New System.Drawing.Size(113, 20)
+        Me.txtRemplazar.TabIndex = 1
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(22, 136)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(113, 23)
+        Me.btnBuscar.TabIndex = 2
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnRemplazar
+        '
+        Me.btnRemplazar.Location = New System.Drawing.Point(22, 168)
+        Me.btnRemplazar.Name = "btnRemplazar"
+        Me.btnRemplazar.Size = New System.Drawing.Size(113, 23)
+        Me.btnRemplazar.TabIndex = 3
+        Me.btnRemplazar.Text = "Remplazar"
+        Me.btnRemplazar.UseVisualStyleBackColor = True
+        '
+        'btnRemplazarT
+        '
+        Me.btnRemplazarT.Location = New System.Drawing.Point(22, 200)
+        Me.btnRemplazarT.Name = "btnRemplazarT"
+        Me.btnRemplazarT.Size = New System.Drawing.Size(113, 23)
+        Me.btnRemplazarT.TabIndex = 4
+        Me.btnRemplazarT.Text = "Remplazar todo"
+        Me.btnRemplazarT.UseVisualStyleBackColor = True
+        '
+        'lblCoincidencia
+        '
+        Me.lblCoincidencia.AutoSize = True
+        Me.lblCoincidencia.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCoincidencia.Location = New System.Drawing.Point(9, 327)
+        Me.lblCoincidencia.Name = "lblCoincidencia"
+        Me.lblCoincidencia.Size = New System.Drawing.Size(137, 15)
+        Me.lblCoincidencia.TabIndex = 5
+        Me.lblCoincidencia.Text = "0 Palabras encontradas"
+        '
+        'lblLinCol
+        '
+        Me.lblLinCol.AutoSize = True
+        Me.lblLinCol.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLinCol.Location = New System.Drawing.Point(31, 14)
+        Me.lblLinCol.Name = "lblLinCol"
+        Me.lblLinCol.Size = New System.Drawing.Size(130, 17)
+        Me.lblLinCol.TabIndex = 0
+        Me.lblLinCol.Text = "Linea 1  Columna 1"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(20, 16)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(669, 335)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(661, 309)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(871, 358)
+        Me.ClientSize = New System.Drawing.Size(893, 448)
+        Me.Controls.Add(Me.SplitContainer1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.PerformLayout()
+        Me.SplitContainer2.ResumeLayout(False)
+        Me.TabControl1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -60,4 +259,22 @@ Partial Class Form1
 
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NuevoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GuardarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GuardarComoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GenerarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ErroresToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents SplitContainer2 As SplitContainer
+    Friend WithEvents lblCoincidencia As Label
+    Friend WithEvents btnRemplazarT As Button
+    Friend WithEvents btnRemplazar As Button
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents txtRemplazar As TextBox
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents lblLinCol As Label
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage2 As TabPage
 End Class
